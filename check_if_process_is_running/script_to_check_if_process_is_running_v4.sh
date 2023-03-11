f4fd2610ed737d529135b5996cdd9657926c94b6
100daysofdevops <100daysofdevops@gmail.com>
2023-03-11 08:20:25 -0800

    Updated script with a logic to check the user

---
+++ check_if_process_is_running/script_to_check_if_process_is_running_v4.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Email settings
+EMAIL_SUBJECT="Process not running"
+EMAIL_TO="you@example.com"
+EMAIL_FROM="alert@example.com"
+
+# Check if the user running the script is not root
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+function send_email {
+  # Send email using the mail command
+  echo "$1" | mail -s "$EMAIL_SUBJECT" -r "$EMAIL_FROM" "$EMAIL_TO"
+}
+
+function check_process {
+  # Check if the process name was provided
+  if [ -z "$1" ]; then
+    echo "Please provide the process name."
+    return 1
+  fi
+
+  # Check if the process is running for the current user
+  if pgrep -x "$1" -u "$(id -u)" >/dev/null; then
+    echo "Process '$1' is running for user $(whoami)."
+    return 0
+  else
+    echo "Process '$1' is not running for user $(whoami). Attempting to restart..."
+    # Restart the process
+    sudo systemctl restart $1
+    sleep 5
+
+    # Check if the process is running after restart
+    if pgrep -x "$1" -u "$(id -u)" >/dev/null; then
+      echo "Process '$1' restarted successfully for user $(whoami)."
+      return 0
+    else
+      echo "Failed to restart process '$1' for user $(whoami). Sending email notification..."
+      send_email "Failed to restart process '$1' for user $(whoami). Please investigate."
+      return 1
+    fi
+  fi
+}
+
+check_process "$1"
+
