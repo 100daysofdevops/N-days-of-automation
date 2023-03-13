68c9213eb803d0bb7d6e6ebbd484b79ac0f95e8e
DevFzn <125571873+DevFzn@users.noreply.github.com>
2023-03-12 23:02:47 -0300

    Initiate optimizations while maintaining readability
    Some changes over test usage, and definition.
    Avoid check read permission if file/dir doesn't exist.

--- check_if_file_directory_present/test_check_if_file_directory_present.sh
+++ check_if_file_directory_present/test_check_if_file_directory_present.sh
@@ -1,103 +1,89 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Copy the check_file_or_dir function from the script
+RED_COLOR='\e[0;31m'
+GRN_COLOR='\e[0;32m'
+RST_COLOR='\e[0m'
+
+# Function to check if file or directory exists
 check_file_or_dir(){
-    local input=$1
-    local output=""
-
-    if [ ! -e "$input" ]
-    then
-        output="File or directory $input not present"
-    elif [ -d "$input" ]
-    then
-        output="The entered input $input is a directory"
-    elif [ -f "$input" ]
-    then
-        output="The entered input $input is a file"
+    INPUT="${1}"
+    if [ ! -e "$INPUT" ]; then
+        OUTPUT="File or directory $INPUT not present"
+    elif [ -d "$INPUT" ]; then
+        OUTPUT="The entered input $INPUT is a directory"
+        EXISTS=1
+    elif [ -f "$INPUT" ]; then
+        OUTPUT="The entered input $INPUT is a file"
+        EXISTS=1
     else
-        output="The entered input $input is neither a file nor a directory"
+        OUTPUT="The entered input $INPUT is neither a file nor a directory"
     fi
 
-    if [ ! -r "$input" ]
-    then
-        output+="; You don't have permission to read $input"
+    # If file/dir not exists, don't check read permission
+    if [[ ${EXISTS} -gt 0 ]]; then
+        if [ ! -r "${1}" ]; then
+            OUTPUT+="; You don't have permission to read ${1}"
+        fi
     fi
-
-    echo "$output"
+    echo "$OUTPUT"
     return 0
 }
 
-
-# Run the Test that the function correctly identifies files and directories
-test1() {
-    expected="The entered input test.txt is a file"
-    input="test.txt"
-
-    output=$(check_file_or_dir "$input")
-
-    if [ "$output" != "$expected" ]; then
-        echo -e "\033[0;31mTest failed: expected '$expected' but got '$output'\033[0m"
+# Set test report function
+test_report() {
+    OUTPUT=$(check_file_or_dir "${1}")
+    EXPECTED="${2}"
+    if [[ "$OUTPUT" != "$EXPECTED" ]]; then
+        printf "%b Test failed: expected '%s' but got '%s'%b\n" \
+               "${RED_COLOR}" "${EXPECTED}" "${OUTPUT}" "${RST_COLOR}"
         return 1
     else
-        echo -e "\033[0;32mTest passed\033[0m"
+        printf '%bTest passed%b\n' "${GRN_COLOR}" "${RST_COLOR}"
         return 0
     fi
+}
+
+# Set tests
+test1() {
+    EXPECTED="The entered input test.txt is a file"
+    INPUT="test.txt"
+    test_report "${INPUT}" "${EXPECTED}"
 }
 
 test2() {
-    expected="The entered input /proc/1 is a directory"
-    input="/proc/1"
-
-    output=$(check_file_or_dir "$input")
-
-    if [ "$output" != "$expected" ]; then
-        echo -e "\033[0;31mTest failed: expected '$expected' but got '$output'\033[0m"
-        return 1
-    else
-        echo -e "\033[0;32mTest passed\033[0m"
-        return 0
-    fi
+    EXPECTED="The entered input /proc/1 is a directory"
+    INPUT="/proc/1"
+    test_report "${INPUT}" "${EXPECTED}"
 }
 
 test3() {
-    expected="You don't have permission to read /etc/shadow"
-    input="/etc/shadow"
-
-    output=$(check_file_or_dir "$input")
-
-    if [ "$output" != "$expected" ]; then
-        echo -e "\033[0;31mTest failed: expected '$expected' but got '$output'\033[0m"
-        return 1
-    else
-        echo -e "\033[0;32mTest passed\033[0m"
-        return 0
-    fi
+    EXPECTED="The entered input /etc/shadow is a file"
+    EXPECTED+="; You don't have permission to read /etc/shadow"
+    INPUT="/etc/shadow"
+    test_report "${INPUT}" "${EXPECTED}"
 }
 
 test4() {
-    expected="File or directory xyz.txt not present"
-    input="xyz.txt"
-
-    output=$(check_file_or_dir "$input")
-
-    if [ "$output" != "$expected" ]; then
-        echo -e "\033[0;31mTest failed: expected '$expected' but got '$output'\033[0m"
-        return 1
-    else
-        echo -e "\033[0;32mTest passed\033[0m"
-        return 0
-    fi
+    EXPECTED="File or directory xyz.txt not present"
+    INPUT="xyz.txt"
+    test_report "${INPUT}" "${EXPECTED}"
 }
 
+# Declare tests array
+TESTS=( test1
+        test2
+        test3
+        test4 )
+
 # Run the tests
-test1
-test2
-test3
-test4
+for TEST in ${TESTS[@]}; do
+    $TEST
+    TEST_RESULTS+="$?"
+done
 
 # Check the exit codes of the tests
-if [ "$?" -eq 0 ]; then
-    echo "All tests passed"
-else
+if [[ ${TEST_RESULTS[@]} =~ 1 ]]; then
     echo "Some tests failed"
+else
+    echo "All tests passed"
 fi
